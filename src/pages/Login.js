6dbfe308daa8b1627ccbb4b26f33c39f4a1c1d5a
RomanIrshad123 <romanirshad1@gmail.com>
2026-04-02 17:01:39 -0400

    Update API endpoint for login authentication

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await API.post("/auth/login", { email, password });
+      const res = await API.post("/api/auth/login", { email, password });
 
       localStorage.setItem("token", res.data.token);
       setAuthToken(res.data.token);
